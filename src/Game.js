1ee5437d430f486a7da835a271d5e5fc00c50848
Rahul <64414414+RahulBansal123@users.noreply.github.com>
2020-12-16 01:45:18 +0530

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,38 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import './Game.css';
+import game from './Game.css';
 
 const Game = () => {
+  // Initialising states with default values
   const [word, setWord] = useState('');
   const [value, setValue] = useState('');
   const [err, setErr] = useState('');
   const [success, setSuccess] = useState('');
   const [score, setScore] = useState(0);
-  const [scrambledWord, setScrambledWord] = useState('');
+  const [question, setQuestion] = useState('');
 
+  // Starts generating words as soon as
+  // the component is placed in the DOM Tree
   useEffect(() => {
     wordGenerate();
   }, []);
 
-  const WORDS = [
-    'GeeksForGeeks',
-    'Data Structures and algorithms',
-    'React',
-    'Sandeep Jain',
-  ];
-
-  const SCRAMBLED = [
+  // Defining questions with their
+  // corresponding answers
+  const QUESTION = [
     'Greatest Computer Science portal for geeks',
     'Full form of DSA',
     'Most Used Web Framework',
     'Founder of GeeksForGeeks',
+    'Who Created Facebook',
+    'CEO of Tesla and SpaceX',
+    'When 5G going to launch in India',
   ];
 
+  const ANSWERS = [
+    'GeeksForGeeks',
+    'Data Structures and algorithms',
+    'React',
+    'Sandeep Jain',
+    'Mark Zuckerberg',
+    'Elon Musk',
+    '2021',
+  ];
+
+  // Generating Random questions and storing
+  // their answers in the state variable
   const wordGenerate = () => {
-    const randomNumber = Math.floor(Math.random() * WORDS.length);
-    setWord(WORDS[randomNumber]);
-    setScrambledWord(SCRAMBLED[randomNumber]);
+    const randomNumber = Math.floor(Math.random() * ANSWERS.length);
+    setWord(ANSWERS[randomNumber]);
+    setQuestion(QUESTION[randomNumber]);
   };
 
+  // checking if the typed answer is correct or not
   const check = () => {
     if (value) {
       if (value.toLowerCase() === word.toLowerCase()) {
@@ -45,6 +59,8 @@ const Game = () => {
         setSuccess(null);
         setErr('Wrong Answer');
         setScore((prevScore) => prevScore - 1);
+        setValue('');
+        wordGenerate();
       }
     } else {
       setSuccess(null);
@@ -52,6 +68,8 @@ const Game = () => {
     }
   };
 
+  // defining function to skip
+  // to another question
   const reset = () => {
     setValue(null);
     setSuccess(null);
@@ -59,30 +77,30 @@ const Game = () => {
     wordGenerate();
   };
 
+  // Writing the JSX Code
   return (
     <div>
-      <p className="Game__score">SCORE : {score}</p>
-
-      {scrambledWord ? (
-        <p className="Game__scrambled_word">Word: {scrambledWord}</p>
+      <p className={game.Game__score}>SCORE : {score}</p>
+      {question ? (
+        <p className={game.Game__question}>Question: {question}</p>
       ) : null}
 
-      {err ? <p className="Game__error">{err}</p> : null}
+      {err ? <p className={game.Game__error}>{err}</p> : null}
 
-      {success ? <p className="Game__success">{success}</p> : null}
+      {success ? <p className={game.Game__success}>{success}</p> : null}
 
       <input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
         placeholder="Enter Answer Here!"
-        className="Game__answer"
+        className={game.Game__input_answer}
       />
 
-      <button onClick={reset} className="Game__start">
+      <button onClick={reset} className={game.Game__start}>
         Reset
       </button>
-      <button onClick={check} className="Game__submit">
+      <button onClick={check} className={game.Game__submit}>
         Enter
       </button>
     </div>
