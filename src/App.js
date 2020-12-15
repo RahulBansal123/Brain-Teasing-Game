069c51860d7d40d6f1fda67761547071f6ca1384
Rahul <64414414+RahulBansal123@users.noreply.github.com>
2020-12-16 01:44:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Game from './Game';
+import React from 'react';
+import app from './App.css';
+import Game from './Game.js';
 
 function App() {
+  // Creating the container and importing
+  // Game component
   return (
-    <div className="Game__main_container">
-      <h1>Welcome to the Brain Twister game</h1>
-      <div className="Game__container">
+    <div className={app.Game__main_container}>
+      <h1 className={app.Game__heading}>Welcome to the Brain Twister game</h1>
+      <div className={app.Game__container}>
         <Game />
       </div>
     </div>
